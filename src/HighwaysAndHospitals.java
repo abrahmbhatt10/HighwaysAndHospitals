000d8eb95bd88df696b6ed4f9d9c676cd29306e8
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2024-09-10 21:21:37 -0700

    code

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,4 +1,5 @@
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -20,36 +21,37 @@ public class HighwaysAndHospitals {
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         long minCost = 0;
         long mCost;
-        int numHospitals;
-        Queue<Integer> hCities = new LinkedList<Integer>();
-        Queue<Integer> connectedCities = new LinkedList<Integer>();
+        Map<Integer, Integer> hCities = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> connectedCities = new HashMap<Integer, Integer>();
         int maxValue = n+1;
         int mostConnectedCity;
+        int connectedCityNum;
         for(int i = 0; i < cities.length; i++){
             mostConnectedCity = getMostConnectedCity(maxValue, cities);
-            if()
-            hCities.add(mostConnectedCity);
-            addConnectedCity(connectedCities, cities, mostConnectedCity);
-            maxValue = getNumOfConnectedCities(mostConnectedCity,cities);
-        }
-        for(int i = 0; i < )
-        for(int numHighways = 0; numHighways < maxHighways(cities) + 1; numHighways++){
-            if(numHighways <= n){
-                numHospitals = n - numHighways;
+            connectedCityNum = getNumOfConnectedCities(mostConnectedCity, cities);
+            if(connectedCities.containsKey(mostConnectedCity)){
+                addConnectedCities(connectedCities, cities, mostConnectedCity);
             }
             else{
-                numHospitals = 1;
+                if(!(hCities.containsKey(mostConnectedCity)))
+                {
+                    hCities.put(mostConnectedCity, connectedCityNum);
+                    if(!(connectedCities.containsKey(mostConnectedCity))){
+                        addConnectedCities(connectedCities, cities, mostConnectedCity);
+                    }
+                }
             }
-            mCost = costForSolution(n, cities, hospitalCost, highwayCost, numHospitals, numHighways);
-            if((mCost < minCost) || (minCost == 0)){
-                minCost = mCost;
+            maxValue = connectedCityNum;
+        }
+        for(int i = 1; i < n+1; i++){
+            if(!hCities.containsKey(i) && !connectedCities.containsKey(i)){
+                hCities.put(i, 0);
             }
         }
+        int numHospitals = hCities.size();
+        int numHighways = cities.length;
+        minCost = (numHospitals * hospitalCost) + (numHighways * highwayCost);
         return minCost;
-    }
-
-    public static boolean isExists(){
-
     }
 
     public static int getNumOfConnectedCities(int cityNumber, int cities[][]){
@@ -62,13 +64,13 @@ public class HighwaysAndHospitals {
         return answer;
     }
 
-    public static void addConnectedCity(Queue<Integer> connectedCities, int cities[][], int rootCityNumber){
+    public static void addConnectedCities(Map<Integer, Integer> connectedCities, int cities[][], int rootCityNumber){
         for(int i = 0; i < cities.length; i++){
             if(cities[i][0] == rootCityNumber){
-                connectedCities.add(cities[i][1]);
+                connectedCities.put(cities[i][1], rootCityNumber);
             }
             if(cities[i][1] == rootCityNumber){
-                connectedCities.add(cities[i][0]);
+                connectedCities.put(cities[i][0], rootCityNumber);
             }
         }
     }
