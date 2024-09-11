dd2843e445bd105659a1512f4de92347a60d8f66
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2024-09-11 10:55:41 -0700

    code

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,13 +1,14 @@
 Name: YOUR NAME HERE
 
-| Date        |    Time     |                                                                                                                                                                          Update |
-|:------------|:-----------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
-| September 9 |  5-5:30pm   |                                                                                                                                                   I brainstormed possible code. |
-| September 9 | 5:30-6:00pm |                                                                                                                                                   I tried using a math formula. |
-| September 9 | 6:00-6:30pm |                                                                                                                           The formula worked, but only for the small test case. |
-| September 9 |  6:30-7pm   |                                                                                                                                        I tried to brainstorm new possibilities. |
-| September 9 |  7-7:30pm   |                                                                            I contemplated using Breadth First Search, but decided to wait until more clues were given in class. |
-| September 9 |  7:30-8pm   |  I thought that this problem set has two dice roll set, one in which there was a hospital or not in a city, whether there was a highway, no high way, or whether it was costly. |
+| Date         |    Time     |                                                                                                                                                                         Update |
+|:-------------|:-----------:|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| September 9  |  5-5:30pm   |                                                                                                                                                  I brainstormed possible code. |
+| September 9  | 5:30-6:00pm |                                                                                                                                                  I tried using a math formula. |
+| September 9  | 6:00-6:30pm |                                                                                                                          The formula worked, but only for the small test case. |
+| September 9  |  6:30-7pm   |                                                                                                                                       I tried to brainstorm new possibilities. |
+| September 9  |  7-7:30pm   |                                                                           I contemplated using Breadth First Search, but decided to wait until more clues were given in class. |
+| September 9  |  7:30-8pm   | I thought that this problem set has two dice roll set, one in which there was a hospital or not in a city, whether there was a highway, no high way, or whether it was costly. |
+| September 10 |  5-5:30pm   |                                                                                                                                                    I thought that finding the  |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
@@ -25,30 +26,50 @@ public class HighwaysAndHospitals {
         int maxValue = n+1;
         int mostConnectedCity;
         int connectedCityNum;
+        int [] numConnectedCities = new int[n+1];
+        for(int i = 1; i < n+1; i++)
+        {
+            numConnectedCities[i] = getNumOfConnectedCities(i,cities);
+        }
         for(int i = 0; i < cities.length; i++){
-            mostConnectedCity = getMostConnectedCity(maxValue, cities);
+            mostConnectedCity = getMostConnectedCity(maxValue, numConnectedCities);
             connectedCityNum = getNumOfConnectedCities(mostConnectedCity, cities);
+            System.out.println("Most connected "+mostConnectedCity+" with "+connectedCityNum);
+            if(mostConnectedCity == 0 || connectedCityNum == 0)
+            {
+                break;
+            }
             if(connectedCities.containsKey(mostConnectedCity)){
-                addConnectedCities(connectedCities, cities, mostConnectedCity);
+                addConnectedCities(hCities, connectedCities, cities, mostConnectedCity);
             }
             else{
                 if(!(hCities.containsKey(mostConnectedCity)))
                 {
                     hCities.put(mostConnectedCity, connectedCityNum);
                     if(!(connectedCities.containsKey(mostConnectedCity))){
-                        addConnectedCities(connectedCities, cities, mostConnectedCity);
+                        addConnectedCities(hCities, connectedCities, cities, mostConnectedCity);
                     }
                 }
             }
-            maxValue = connectedCityNum;
+            System.out.println("Hospitals "+ hCities.toString());
+            System.out.println("Hightways "+ connectedCities.toString());
         }
         for(int i = 1; i < n+1; i++){
             if(!hCities.containsKey(i) && !connectedCities.containsKey(i)){
                 hCities.put(i, 0);
             }
         }
+        System.out.println("Hospitals "+ hCities.toString());
+        System.out.println("Hightways "+ connectedCities.toString());
+        System.out.println("NumCost "+hospitalCost+" "+highwayCost);
+        String citiesStr = "";
+        for(int i =0; i < cities.length; i++)
+        {
+            citiesStr += "-"+cities[i][0]+"_"+cities[i][1];
+        }
+        System.out.println(citiesStr);
         int numHospitals = hCities.size();
-        int numHighways = cities.length;
+        int numHighways = connectedCities.size();
         minCost = (numHospitals * hospitalCost) + (numHighways * highwayCost);
         return minCost;
     }
@@ -63,33 +84,32 @@ public class HighwaysAndHospitals {
         return answer;
     }
 
-    public static void addConnectedCities(Map<Integer, Integer> connectedCities, int cities[][], int rootCityNumber){
+    public static void addConnectedCities(Map<Integer, Integer> hCities, Map<Integer, Integer> connectedCities, int cities[][], int rootCityNumber){
+        System.out.println("Adding connected for "+rootCityNumber);
         for(int i = 0; i < cities.length; i++){
-            if(cities[i][0] == rootCityNumber){
+            System.out.println("*"+cities[i][0]+"**"+cities[i][1]);
+            if((cities[i][0] == rootCityNumber) && (!connectedCities.containsKey(cities[i][1])) && (!hCities.containsKey(cities[i][1]))){
                 connectedCities.put(cities[i][1], rootCityNumber);
             }
-            if(cities[i][1] == rootCityNumber){
+            if((cities[i][1] == rootCityNumber) && (!connectedCities.containsKey(cities[i][0])) && (!hCities.containsKey(cities[i][0]))) {
                 connectedCities.put(cities[i][0], rootCityNumber);
             }
         }
     }
 
-    public static int getMostConnectedCity(int maxValue, int cities[][]){
+    public static int getMostConnectedCity(int maxValue, int numConnectedCities[]){
         int answer = 0;
         int mostConnected = 0;
         int mostConnectedCity = 0;
-        for(int i = 0; i < cities.length; i++){
-            answer = getNumOfConnectedCities(cities[i][0], cities);
-            if((mostConnected < answer) && (answer < maxValue)){
+        for(int i = 1; i < numConnectedCities.length; i++)
+        {
+            answer = numConnectedCities[i];
+            if((answer != -1) && (mostConnected < answer) && (answer < maxValue)){
                 mostConnected = answer;
-                mostConnectedCity = cities[i][0];
-            }
-            answer = getNumOfConnectedCities(cities[i][1], cities);
-            if((mostConnected < answer) && (answer < maxValue)){
-                mostConnected = answer;
-                mostConnectedCity = cities[i][1];
+                mostConnectedCity = i;
             }
         }
+        numConnectedCities[mostConnectedCity] = -1;
         return mostConnectedCity;
     }
 
